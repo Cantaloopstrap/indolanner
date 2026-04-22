391b050fe0d237b6c3b11f1fad7e39f34a5ca022
belpythons <zerobocilicung393@gmail.com>
2026-04-23 00:38:37 +0800

    chore(deps): update dependencies and next.js configurations

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,20 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+      },
+    ],
   },
 }
 
